0644aa923d564a30786f88252b2c167a19bf7337
Ria <benedek.maria88@gmail.com>
2021-09-04 11:39:27 +0200

    created Element

---
+++ src/main/java/application/models/Element.java
@@ -0,0 +1,12 @@
+package application.models;
+
+public class Element {
+
+    private String elementName;
+    private byte[] icon_img;
+
+    public Element(String elementName) {
+        this.elementName = elementName;
+    }
+
+}
