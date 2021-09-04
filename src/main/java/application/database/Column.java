67ca99de4b62488c080fba46bef6c7ea359ff5d4
Ria <benedek.maria88@gmail.com>
2021-09-04 16:28:41 +0200

    created enum Column

---
+++ src/main/java/application/database/Column.java
@@ -0,0 +1,20 @@
+package application.database;
+
+public enum Column {
+
+    ID,
+    UNIQUE_NAME,
+    DRAGON_TEXT,
+    RARITY,
+    DESIGN,
+
+    DRAGON_ID,
+    ELEMENT_NAME,
+
+    ICON_IMG;
+
+    public String dbName() {
+        return name().toLowerCase();
+    }
+
+}
