047f829bda88bc9bb6a33dfdb586d989a9d2e391
Ria <benedek.maria88@gmail.com>
2021-09-04 17:04:06 +0200

    added column numbers to enum Table

--- src/main/java/application/database/Table.java
+++ src/main/java/application/database/Table.java
@@ -2,8 +2,14 @@ package application.database;
 
 public enum Table {
 
-    DRAGON,
-    ELEMENT,
-    DRAGONS_ELEMENT
+    DRAGON(5),
+    ELEMENT(3),
+    DRAGONS_ELEMENT(2);
+
+    public final int COL_NUM;
+
+    Table(int colNum) {
+        COL_NUM = colNum;
+    }
 
 }
