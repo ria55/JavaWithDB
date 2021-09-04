00ab38ff228ea71ac6ff9c2453b7317865c56469
Ria <benedek.maria88@gmail.com>
2021-09-04 17:17:18 +0200

    tested QueryBuilder

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,6 +1,9 @@
 package application;
 
+import application.database.Column;
 import application.database.DBEngine;
+import application.database.QueryBuilder;
+import application.database.Table;
 import application.models.Dragon;
 import application.models.Rarity;
 
@@ -9,21 +12,32 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        DBEngine engine = new DBEngine();       // should connect
+        /*DBEngine engine = new DBEngine();       // should connect
 
         if (engine.isConnected()) {
+            Dragon dragon = new Dragon("Marcipán", "Cuki sárkány, edd meg!", Rarity.HEROIC);
+            boolean success = engine.addDragonToDB(dragon);
+            System.out.println(success);
+            System.out.println();
             List<Dragon> dragons = engine.listAllDragons();
 
-            for (Dragon dragon : dragons) {
-                System.out.println(dragon);
+            for (Dragon d : dragons) {
+                System.out.println(d);
             }
-
-            /*Dragon dragon = new Dragon("Paff", "A bűvös sárkány", Rarity.HEROIC);
-            boolean success = engine.addDragonToDB(dragon);
-            System.out.println(success);*/
         } else {
             System.out.println("no connection");
-        }
+        }*/
+
+        String query1 = new QueryBuilder().select(Table.DRAGON).where(Column.UNIQUE_NAME, true).build();
+        String query2 = new QueryBuilder().select(Table.DRAGON, Column.ID).where(Column.ID, false).build();
+        String query3 = new QueryBuilder().insert(Table.DRAGON).build();
+        String query4 = new QueryBuilder().insert(Table.DRAGON, Column.UNIQUE_NAME, Column.DRAGON_TEXT).build();
+
+        System.out.println(query1);
+        System.out.println(query2);
+        System.out.println(query3);
+        System.out.println(query4);
+
     }
 
 }
