614467f02739d7eeeed08758b1350a883cfed05e
Ria <benedek.maria88@gmail.com>
2021-09-04 11:46:42 +0200

    getters, setters, default constructor

--- src/main/java/application/models/Dragon.java
+++ src/main/java/application/models/Dragon.java
@@ -12,6 +12,8 @@ public class Dragon {
 
     private List<Element> elements;
 
+    public Dragon() {}
+
     public Dragon(long id, String uniqueName, String dragonText, Rarity rarity) {
         this.id = id;
         this.uniqueName = uniqueName;
@@ -19,4 +21,51 @@ public class Dragon {
         this.rarity = rarity;
     }
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUniqueName() {
+        return uniqueName;
+    }
+
+    public void setUniqueName(String uniqueName) {
+        this.uniqueName = uniqueName;
+    }
+
+    public String getDragonText() {
+        return dragonText;
+    }
+
+    public void setDragonText(String dragonText) {
+        this.dragonText = dragonText;
+    }
+
+    public Rarity getRarity() {
+        return rarity;
+    }
+
+    public void setRarity(Rarity rarity) {
+        this.rarity = rarity;
+    }
+
+    public byte[] getDesign() {
+        return design;
+    }
+
+    public void setDesign(byte[] design) {
+        this.design = design;
+    }
+
+    public List<Element> getElements() {
+        return elements;
+    }
+
+    public void setElements(List<Element> elements) {
+        this.elements = elements;
+    }
 }
--- src/main/java/application/models/Element.java
+++ src/main/java/application/models/Element.java
@@ -5,8 +5,25 @@ public class Element {
     private String elementName;
     private byte[] icon_img;
 
+    public Element() {}
+
     public Element(String elementName) {
         this.elementName = elementName;
     }
 
+    public String getElementName() {
+        return elementName;
+    }
+
+    public void setElementName(String elementName) {
+        this.elementName = elementName;
+    }
+
+    public byte[] getIcon_img() {
+        return icon_img;
+    }
+
+    public void setIcon_img(byte[] icon_img) {
+        this.icon_img = icon_img;
+    }
 }
